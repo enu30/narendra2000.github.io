9fb0a6f87074c927e8a58787a98e799b4c4f147c
narendra2000 <46496466+narendra2000@users.noreply.github.com>
2020-09-15 03:47:35 +0530

    covid-19 changes

---
+++ precache-manifest.eb76c9afabf9cb4118c6a0d0bd732083.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108a71d1a09c2879718e7a85f4b5841d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b90dc84f67ff613fcf03",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "0b3a0e4d2dc28f7b5cf8",
+    "url": "/static/css/main.2dcb55fa.chunk.css"
+  },
+  {
+    "revision": "b90dc84f67ff613fcf03",
+    "url": "/static/js/2.361e0b68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.361e0b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3a0e4d2dc28f7b5cf8",
+    "url": "/static/js/main.cee49c05.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "95fb00c01b278a3a37571beee5be8128",
+    "url": "/static/media/covid_gif.95fb00c0.gif"
+  }
+]);
